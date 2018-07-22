257e79116d91ae5b0341036b832add06d2711bd1
Jazk-Z <zzyamor@gmail.com>
2018-07-22 23:55:47 +0800

    提交模板

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const Koa = require('koa');
 const app = new Koa();
+const ejs = require('ejs');
+const pug = require('pug');
+const {ejsTmp, normal, pugTmp} = require('./server/tpl')
 app.use(async (ctx, next) => {
-    ctx.body = '电影首页'
+    ctx.type='text/html; charset=utf-8';
+    ctx.body = ejs.render(ejsTmp, {
+        you: '百岁山',
+        me: '王聪聪'
+    })
+    ctx.body = pug.render(ejsTmp, {
+        you: '百岁山',
+        me: '王聪聪'
+    })
 });
 
-app.listen(4455);
+app.listen(4455, () => {
+    console.log(`server is running localhost:4455`)
+});
---
+++ server/tpl/index.js
@@ -0,0 +1,9 @@
+const HTMLTmp = require('./html');
+const EJSTmp = require('./ejs');
+const PUGTmp = require('./pug');
+
+module.exports = {
+    normal: HTMLTmp,
+    ejsTmp: EJSTmp,
+    pugTmp: PUGTmp
+}
